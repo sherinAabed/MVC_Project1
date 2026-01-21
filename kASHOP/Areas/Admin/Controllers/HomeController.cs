4ebc8a365769c761bc8760a05ccd9fcead1fd064
sherinAabed <1211462@student.birzeit.edu>
2026-01-21 18:20:52 +0200

    init projrct

---
+++ kASHOP/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kASHOP.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+            //return View(); =>      return View("Index");
+            //Action name same as View name > so we can leave it empty
+            //return Content("this is Home for Admin "); => if we want  RETURN STRING : USE Content
+        }
+    }
+}
